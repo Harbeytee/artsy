3e1261e7ede83112eae5941ec134e646dc675308
Abby <harbeyteejay@gmail.com>
2023-01-21 23:42:20 +0100

    styled featured product section

--- src/pages/home/Upcoming.jsx
+++ src/pages/home/Upcoming.jsx
@@ -11,28 +11,31 @@ export default function Upcoming() {
         </div>
 
         <div className="upcoming">
-
-          <div className='upcoming-title'>
+          <div className='card'>
+            <div className='upcoming-title'>
             <span></span>
-            <h3>MONALISA REDEFINED IN STYLE.</h3>
+              <h3>MONALISA REDEFINED IN STYLE.</h3>
+            </div>
+
+            <div>
+              <span className='one'>01</span>
+              <p className='time'>START ON : 08:00 GTS . MONDAY </p>
+            </div>
+
+          
+            <p style={{color: 'white'}}>GET EXCLUSIVE VIEWING OF CONTEMPORARY ART AND CONNECT 
+              WITH INVESTORS AND AUCTIONEERS ACROSS 
+              THE WORLD BRINGING THEIR HIGHEST AND LOWEST BIDS.
+            </p>
           </div>
+            
 
-          <div>
-            <span className='one'>01</span>
-            <span className='time'>Start on : 08:00 GTS . Monday </span>
-          </div>
-
-          <p style={{color: 'white'}}>GET EXCLUSIVE VIEWING OF CONTEMPORARY ART AND CONNECT 
-            WITH INVESTORS AND AUCTIONEERS ACROSS 
-            THE WORLD BRINGING THEIR HIGHEST AND LOWEST BIDS.
-          </p>
-
-          <div>
-            <Link style={{color: 'white', textDecoration: 'none'}}>See more</Link>
+          <div className='links'>
+            <Link style={{color: 'white'}}>See more</Link>
             <button>Set a reminder</button>
           </div>
-
         </div>
+        
         
       </article>
         <div>
