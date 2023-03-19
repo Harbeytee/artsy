ce4bba665fd867e6de714fca0f7c48170709928b
Abby <harbeyteejay@gmail.com>
2023-03-19 22:59:53 +0100

    fixed bugs

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect} from 'react'
 import CartHead from './components/CartHead'
 import { Link, Outlet } from 'react-router-dom'
 import { Context } from '../../Context/Context'
+import cartIcon from '../../assets/icons/cart-icon.svg'
 export default function Cart({turnOn, turnOff, index, changeIndex2}) {
   const { art, changeIndex} = useContext(Context)
 
@@ -23,7 +24,15 @@ export default function Cart({turnOn, turnOff, index, changeIndex2}) {
         <Outlet />
         </>
         :
-          <div>nothing dey here g</div>
+          
+          <div id='empty-cart'>
+            <img src={cartIcon} alt="icon of an empty cart" />
+            <h1>Your cart is empty</h1>
+            <p>Lets go buy something</p>
+            <Link to='/marketplace'>
+              Shop Now
+            </Link>
+          </div>
         }
 
         
